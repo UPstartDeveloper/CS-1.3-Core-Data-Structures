bbf826ff721940f3f003ab1cf842e5fc9c6682f4
= <zainr7989@gmail.com>
2020-02-04 09:48:53 -0800

    Implement get_at_index method.

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -79,7 +79,12 @@ class LinkedList(object):
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index < self.size):
             raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node at the given index and return its data
+        # Find the node at the given index
+        node = self.head
+        for i in range(index):
+            node = node.next
+        # return its data
+        return node.data
 
     def insert_at_index(self, index, item):
         """Insert the given item at the given index in this linked list, or
