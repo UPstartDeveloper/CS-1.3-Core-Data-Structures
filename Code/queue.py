92c8af04520cee07e282b27ec8276c8124da30bc
= <zainr7989@gmail.com>
2020-02-05 18:50:43 -0800

    Implement TODO's for LinkedQueue class.

--- Code/queue.py
+++ Code/queue.py
@@ -21,27 +21,36 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        return self.list.length() == 0
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        # TODO: Return front item, if any
+        front = self.list.head
+        if front is None:
+            return front
+        else:
+            return front.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any
+        front = self.front()
+        if front is None:
+            raise ValueError('There are no items in this queue.')
+        else:
+            self.list.delete(front)
+            return front
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
