bc84f0639d878bb168988388e1f1422f250f3ed9
= <zainr7989@gmail.com>
2020-02-26 16:56:19 -0800

    Init sets and set_test modules.

---
+++ Code/sets.py
@@ -0,0 +1,6 @@
+from hashtable import hashtable
+
+
+class HashSet:
+    '''A hash table data structure that implements a set abstract data type.'''
+    pass
---
+++ Code/sets_test.py
@@ -0,0 +1,7 @@
+from sets import HashSet
+import unittest
+
+
+class SetTests(unittest.TestCase):
+    '''Unit tests for the HashSet class.'''
+    pass
