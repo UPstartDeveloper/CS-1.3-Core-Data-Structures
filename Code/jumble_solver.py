a14ad736317a7d7a1babffe78a081fd865ee500f
= <zainr7989@gmail.com>
2020-03-02 17:12:59 -0800

    Add unscrambling function.

---
+++ Code/jumble_solver.py
@@ -0,0 +1,17 @@
+def unscramble(word):
+    """Returns a common English word, given a string that consists solely of
+       English letters.
+
+       Parameters:
+       word(str): a str composed entirely of English letters. No other types of
+                  characters can appear. Letters may be repeated.
+                  All lower case. All letters will appear in output str, and
+                  in the same distribution as in the input str.
+
+        Returns:
+        str: the unscrambeld str
+
+    """
+    # store the number of characters in the input str
+    num_letters = len(word)
+    pass
