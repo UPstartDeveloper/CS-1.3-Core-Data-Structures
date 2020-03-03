813c2307d7163a1f8193202c0bc20dfea7715c7e
= <zainr7989@gmail.com>
2020-03-02 17:22:07 -0800

    Add function to pull possible words from.

--- Code/jumble_solver.py
+++ Code/jumble_solver.py
@@ -1,3 +1,17 @@
+def get_words():
+    """Get words from the words file to get anagrams from."""
+    # get words from the words file
+    file = open("/usr/share/dict/words", "r")
+    words_list = file.readlines()
+    file.close()
+    # removing newline characters from strings in words file
+    words_no_newline = list()
+    for word in words_list:
+        word = word[:-3]
+        words_no_newline.append(word)
+    return words_no_newline
+
+
 def unscramble(word):
     """Returns a common English word, given a string that consists solely of
        English letters.
@@ -15,3 +29,6 @@ def unscramble(word):
     # store the number of characters in the input str
     num_letters = len(word)
     pass
+
+
+if __name__ == "__main__":
